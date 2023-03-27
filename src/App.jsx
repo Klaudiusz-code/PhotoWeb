e7bb392765056d757f1bc79d58a90ea10cf9639b
Klaudiusz <muminowy123@gmail.com>
2023-03-27 09:33:33 +0200

    Extension change

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import FilterNAv from "./components/FilterNAv";
+import Navbar from "./components/Navbar";
+import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+        <Navbar/>
+        <Gallery/>
+        <FilterNAv/>
+        <Footer/>
+    </>
+  );
+}
+
+export default App;
