f6dfa5baf54e8a2cf7d2719a808b79612bd85224
Klaudiusz <muminowy123@gmail.com>
2023-03-27 09:20:48 +0200

    Created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="mx-auto bg-zinc-200 p-4 shadow-xl">
+            <div className="container mx-auto lg:flex items-center justify-between  sm:text-center">
+                <h1 className="text-lg text-center">JANE WINELAND - PHOTOGRAPHY &copy; 2023</h1>
+                <span>MADE WITH ❤️ BY K. ADAMASZEK</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
