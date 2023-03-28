b0b60de3fd6dd38cdca94cf6be41e2d0f84e0312
Klaudiusz <muminowy123@gmail.com>
2023-03-28 09:26:28 +0200

    Updated MobileMenu component to use React Router Links

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { AiOutlineClose } from "react-icons/ai";
+import {Link} from "react-router-dom";
 
 const MobileMenu = ({ isMenuOpen, onClose }) => {
+
+    const links = [
+        {name:"HOME",link:"/"},
+        {name:"ABOUT",link:'/about'},
+        {name:"CONTACT",link:"/contact"},
+    ]
     return (
-        <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${isMenuOpen ? "-translate-y-0" : "-translate-y-full"} transition-transform duration-200 ease-in-out filter  `}>
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-white z-50 transform ${isMenuOpen ? "-translate-y-0" : "-translate-y-full"} transition-transform duration-200 ease-in-out filter  `}>
             <div className="flex flex-col justify-center items-center mt-28">
-                <li className="text-2xl font-bold text-light-coral my-4 list-none cursor-pointer">
-                    Home
-                </li>
-                <li className="text-2xl font-bold text-light-coral my-4 list-none cursor-pointer">
-                    About me
-                </li>
-                <li className="text-2xl font-bold text-light-coral my-4 list-none cursor-pointer">
-                    Contact
-                </li>
+                {links?.map((link) =>{
+                    return(
+                        <li className="text-2xl font-bold text-light-coral my-4 list-none cursor-pointer">
+                            <Link to={link.link} onClick={onClose}>{link.name}</Link>
+                        </li>
+                    )
+
+                })}
                 <button className="text-2xl font-bold my-4 hover:text-light-coral fixed right-10 top-0" onClick={onClose}>
                     <AiOutlineClose/>
                 </button>
