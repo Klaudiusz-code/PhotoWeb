8f09931ea661a526362df56cee879b5aa1fae1d9
Klaudiusz <muminowy123@gmail.com>
2023-03-28 08:06:59 +0200

    Style change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="mx-auto bg-zinc-200 p-4 shadow-xl">
-            <div className="container mx-auto lg:flex items-center justify-between  sm:text-center">
+            <div className=" mx-auto lg:flex items-center justify-between  sm:text-center">
                 <h1 className="text-lg text-center">JANE WINELAND - PHOTOGRAPHY &copy; 2023</h1>
                 <span>MADE WITH ❤️ BY K. ADAMASZEK</span>
             </div>
