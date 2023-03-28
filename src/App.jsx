f1c392137dcfc44f1909e3bfac41e274a1d3f001
Klaudiusz <muminowy123@gmail.com>
2023-03-28 08:04:09 +0200

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import FilterNAv from "./components/FilterNAv";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  return (
-    <>
-        <Navbar/>
-        <Gallery/>
-        <FilterNAv/>
-        <Footer/>
-    </>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Gallery />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
