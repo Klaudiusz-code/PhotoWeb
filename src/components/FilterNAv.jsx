83eee0e0b7e384d99fff53c1089e7ebb7f8a556b
Klaudiusz <muminowy123@gmail.com>
2023-03-29 11:19:31 +0200

    Style change

--- src/components/FilterNAv.jsx
+++ src/components/FilterNAv.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 const FilterNav = () => {
     return (
-        <nav className='w-full fixed bottom-3 lg:bottom-10 overflow-hidden z-30'>
-            <div className="container mx-auto py-2">
-                <div className="bg-black/80 max-w-[500px] mx-auto shadow-md lg:flex lg:justify-center rounded-xl sm:flex p-3">
+        <nav className='w-full fixed bottom-10 transition overflow-hidden z-30'>
+            <div className="container  mx-auto py-2">
+                <div className="bg-black/80 max-w-[500px] mx-auto shadow-md flex justify-center rounded-xl  p-3">
                     <li className="list-none px-3 lg:px-8 cursor-pointer">
                         <span className="bg-pink-400 uppercase text-amber-50 text-sm font-semibold px-2 py-2 rounded-xl hover:bg-pink-400 transition">all</span>
                     </li>
                     <li className="list-none px-3 lg:px-8 cursor-pointer">
-                        <span className="uppercase text-amber-50 text-sm font-semibold px-2 py-2 rounded-xl hover:bg-pink-400 transition">lifestyle</span>
+                        <span className="uppercase text-amber-50 lg:text-sm font-semibold px-2 py-2 rounded-xl hover:bg-pink-400 transition">lifestyle</span>
                     </li>
                     <li className="list-none px-3 lg:px-8 cursor-pointer">
                         <span className="uppercase text-amber-50 text-sm font-semibold px-2 py-2 rounded-xl hover:bg-pink-400 transition">corporate</span>
