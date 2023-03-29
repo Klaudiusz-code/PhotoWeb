392f8e4aef3fff5a2f227692e1d749e21505809b
Klaudiusz <muminowy123@gmail.com>
2023-03-29 11:20:30 +0200

    Added style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <footer className="mx-auto bg-zinc-200 p-4 shadow-xl">
             <div className=" mx-auto lg:flex items-center justify-between  sm:text-center">
-                <h1 className="text-lg text-center">JANE WINELAND - PHOTOGRAPHY &copy; 2023</h1>
-                <span>MADE WITH ❤️ BY K. ADAMASZEK</span>
+                <h1 className="lg:text-md md:text-center mob:text-sm">JANE WINELAND - PHOTOGRAPHY &copy; 2023</h1>
+                <span className="lg:text-md mob:text-sm">MADE WITH ❤️ BY K. ADAMASZEK</span>
             </div>
         </footer>
     );
