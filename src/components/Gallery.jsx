c7de922b9d5598a7e6c6f3a002ab6c89541756e6
Klaudiusz <muminowy123@gmail.com>
2023-04-03 15:06:22 +0200

    The gallery_filter method has been added, which filters the displayed images based on the selected category

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,50 +1,69 @@
 import React from 'react';
-import FilterNav from "./FilterNAv";
+import FilterNav from "./FilterNav";
 
+import {useEffect, useState} from "react";
+
+export  const galleryImg = [
+    {name: '1', url: '1.jpg', category: 'testCategory1',},
+    {name: '2', url: '3.jpg', category: 'testCategory1'},
+    {name: '3', url: '4.jpg', category: 'testCategory1'},
+    {name: '4', url: '5.jpg', category: 'testCategory1'},
+    {name: '5', url: '6.jpg', category: 'testCategory1'},
+    {name: '6', url: '7.jpg', category: 'testCategory1'},
+    {name: '7', url: '8.jpg', category: 'testCategory1'},
+    {name: '8', url: '9.jpg', category: 'testCategory1'},
+    {name: '9', url: '10.jpeg', category: 'testCategory2'},
+    {name: '1', url: '11.jpeg', category: 'testCategory2'},
+    {name: '2', url: '12.jpeg', category: 'testCategory2'},
+    {name: '3', url: '13.jpg', category: 'testCategory2'},
+    {name: '4', url: '14.jfif', category: 'testCategory2'},
+    {name: '5', url: '15.jpg', category: 'testCategory2'},
+    {name: '6', url: '16.png', category: 'testCategory2'},
+    {name: '7', url: '17.jpg', category: 'testCategory2'},
+    {name: '8', url: '18.jpg', category: 'testCategory2'},
+    {name: '9', url: '19.jpg', category: 'testCategory2'},
+    {name: '1', url: '20.jpg', category: 'testCategory2'},
+    {name: '1', url: 'ev1.jpg', category: 'testCategory2'},
+    {name: '1', url: 'ev2.jpg', category: 'testCategory2'},
+];
 const Gallery = () => {
 
-    const galleryImg = [
-        {name: 'Lorem ipsum', url: '1.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '3.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '4.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '5.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '6.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '7.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '8.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '9.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '10.jpeg', id: '23'},
-        {name: 'Lorem ipsum', url: '11.jpeg', id: '23'},
-        {name: 'Lorem ipsum', url: '12.jpeg', id: '23'},
-        {name: 'Lorem ipsum', url: '13.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '14.jfif', id: '23'},
-        {name: 'Lorem ipsum', url: '15.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '16.png', id: '23'},
-        {name: 'Lorem ipsum', url: '17.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '18.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '19.jpg', id: '23'},
-        {name: 'Lorem ipsum', url: '20.jpg', id: '23'},
-    ];
+    const [data, setData] = useState([]);
+    const [collection, setCollection] = useState([]);
+
+    useEffect(() => {
+        setData(galleryImg);
+        setCollection([...new Set(galleryImg.map((item) => item.category))]);
+    }, []);
+
+    const gallery_filter = (itemData) => {
+        if (itemData === 'all') {
+            setData(galleryImg);
+        } else {
+            const filterData = galleryImg.filter((item) => item.category === itemData);
+            setData(filterData);
+        }
+    };
 
     return (
         <>
-            <div
-                className="mx-auto lg:mr-24 mt-6 lg:ml-24 lg:columns-3 gap-5 md:columns-2 sm:columns-1 md:mr-4 sm:ml-4 mx-auto space-y-10 pb-28">
-                {galleryImg.map((img) => {
+            <div className="mx-auto lg:mr-24 mt-6 lg:ml-24 lg:columns-3 gap-5 md:columns-2 sm:columns-1 md:mr-4 sm:ml-4 mx-auto space-y-10 pb-28">
+                {data.map((img) => {
                     return (
-                        <div className="bg-gray-200 break-inside-avoid relative cursor-pointer">
-                            <img src={img.url} alt={img.name}/>
+                        <div className="break-inside-avoid relative cursor-pointer" key={img.id}>
+                            <img src={img.url} alt={img.name} className="transition duration-200" />
                             <div
-                                className="absolute bottom-0 left-0 w-full h-full flex items-center flex-wrap justify-center bg-pink-400/30 bg-opacity-75 text-white text-4xl font-bold p-2 transition duration-300 opacity-0 hover:opacity-100">
+                                className="absolute bottom-0 left-0 w-full h-full flex items-center flex-wrap justify-center bg-pink-400/30 bg-opacity-75 text-white text-4xl font-bold p-2 transition duration-300 opacity-0 hover:opacity-100"
+                            >
                                 {img.name}
                             </div>
                         </div>
-                    )
+                    );
                 })}
             </div>
-            <FilterNav/>
+            <FilterNav collection={collection} gallery_filter={gallery_filter} />
         </>
-    )
-
-}
+    );
+};
 
 export default Gallery;
